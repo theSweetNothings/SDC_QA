55ff775986d93d012ed2c5fcffcdb4c40103ead4
CCBaxter84 <troyceclark@gmail.com>
2020-11-12 14:38:12 -0700

    Got React-Bootstrap to work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar.jsx';
+import Button from 'react-bootstrap/Button';
 
 const App = function() {
   return (
     <React.Fragment>
       <SearchBar/>
+      <Button/>
     </React.Fragment>
   );
 };
