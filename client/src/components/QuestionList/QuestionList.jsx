90c039dc9c08eded7b207b3ca94bb2aa1d0cf246
CCBaxter84 <troyceclark@gmail.com>
2020-11-12 17:06:10 -0700

    Added comments to QuestionList.jsx

--- client/src/components/QuestionList/QuestionList.jsx
+++ client/src/components/QuestionList/QuestionList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// This will be updated. I only added it when I was testing SearchBar
 const QuestionList = function() {
   return (
     <section>QuestionList</section>
