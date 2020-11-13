81988b659c3ccf0de3fb9d30da27adc7461a2d94
CCBaxter84 <troyceclark@gmail.com>
2020-11-12 17:21:43 -0700

    Removed axios and useEffect from SearchBar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import styled from 'styled-components';
 
 const StyledContainer = styled.section`
@@ -23,14 +22,6 @@ const StyledInput = styled.input`
 
 const SearchBar = function() {
   const [ searched, setSearched ] = useState('');
-
-  useEffect(() => {
-    if (searched.length >= 3) {
-      axios.get('http://52.26.193.201:3000/qa/5')
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err));
-    }
-  }, [searched]);
 
   const handleChange = function(event) {
     const { value } = event.target;
