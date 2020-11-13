82eabfbdbe7c924662a4a5d1af0c4defac5f96ac
CCBaxter84 <troyceclark@gmail.com>
2020-11-12 18:00:01 -0700

    Updated input unit test for search bar and added comments

--- client/src/components/SearchBar/SearchBar.test.jsx
+++ client/src/components/SearchBar/SearchBar.test.jsx
@@ -5,23 +5,34 @@ import QuestionList from '../QuestionList/QuestionList.jsx';
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
+// Unit Tests for SearchBar component
 describe('SearchBar', () => {
   // Render the searchbar component before each test and then unmount it afterwards
   beforeEach(() => render(<SearchBar/>));
   afterEach(cleanup);
 
-  // Unit Test for SearchBar component
+  // Test whether all children nodes of SearchBar render correctly
   test('renders the SearchBar component with label and placeholder text', () => {
-    // Check that the Label above the searchbar is correct
+    // Check that the Label and the searchbar are present
     expect(screen.getByText('QUESTIONS & ANSWERS')).toBeInTheDocument();
-    // Check that the searchbar <input /> textbox is present
     expect(screen.getByRole('textbox')).toBeInTheDocument();
     // Check that the placeholder text in the searchbar is correct
     expect(screen.getByPlaceholderText('HAVE A QUESTION? SEARCH FOR ANSWERS...')).toBeInTheDocument();
   });
 
-  // Integration test for whether searches update QuestionList component
-  /*test('searches for results after typing 3 or more characters in search bar', () => {
-    render(<QuestionList/>);
-  })*/
+  // Test whether the search bar responds to user input
+  test('updates search bar when user types into textbox', () => {
+    // Confirm user typed input isn't rendered to screen
+    expect(screen.queryByText(/User Input/)).toBeNull();
+    // Simulate user typing 'User Input' into the text box
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'User Input'}
+    });
+    // Check whether 'User Input' has been rendered to the screen in the input box
+    const searchBarText = screen.getByRole('textbox').value;
+    expect(searchBarText).toBe('User Input');
+  });
 });
+
+// Integration Tests for Search Bar
+// Test whether typing 3 or more characters in search bar causes rendered question list to update
