8c11ef89abca054b308d2150ae593c8f272a6df4
CCBaxter84 <troyceclark@gmail.com>
2020-11-21 15:02:50 -0700

    To decrese paint time, removed bootstrap and added compression

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3003;
 const path = require('path');
+const compression = require('compression');
 
 app.use(express.static('../client/dist'));
 app.use(express.json());
+app.use(compression());
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'), err => {
